52369d12a38bf2b5aacb2ee1aca5bb218b1c81ed
VelVic <20610323@utgz.edu.mx>
2025-07-24 11:19:45 -0600

    feat(routing): mejoras en rutas y navegacion, nuevo componente ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,29 @@ import ContactPage from './pages/ContactPage';
 import ShippingPage from './pages/ShippingPage';
 import CartPage from './pages/CartPage';
 import { CartProvider } from './context/CartContext'; // ← IMPORTAR
+import ScrollToTop from './components/ScrollToTop'; // ← AGREGA ESTA LÍNEA
 
 export function App() {
   return (
-    <CartProvider> {/* ← ENVOLVER TODO */}
+    <CartProvider>
       <Router
         future={{
           v7_startTransition: true,
           v7_relativeSplatPath: true
         }}
       >
+        <ScrollToTop /> {/* ← AGREGA ESTA LÍNEA */}
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/productos" element={<ProductsPage />} />
-              <Route path="/product/:id" element={<ProductDetailPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/acerca" element={<AboutPage />} />
+              <Route path="/fragancias" element={<ProductsPage />} />
+              <Route path="/fragancia/:id" element={<ProductDetailPage />} />
+              <Route path="/iniciar-sesion" element={<LoginPage />} />
+              <Route path="/nosotros" element={<AboutPage />} />
               <Route path="/contacto" element={<ContactPage />} />
-              <Route path='/envios' element={<ShippingPage />} />
+              <Route path='/envios-devoluciones' element={<ShippingPage />} />
               <Route path='/carrito' element={<CartPage />} />
             </Routes>
           </main>
